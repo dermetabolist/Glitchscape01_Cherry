53c1fd85696588417ba8dccc461ff5d50ac9083d
Boekemeyer <tboekemeyer@hotmail.de>
2017-05-09 15:55:43 +0200

    added ship controller and lighting

---
+++ Glitchscape01_Cherry/Assets/_Scripts/CamFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour 
+{
+    public GameObject plaerShip;
+
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        Vector3 velocity = Vector3.zero;
+        Vector3 forward = plaerShip.transform.forward * 10.0f;
+        Vector3 needPos = plaerShip.transform.position - forward;
+        transform.position = Vector3.SmoothDamp(transform.position, needPos,
+                                                ref velocity, 0.05f);
+        transform.LookAt(plaerShip.transform);
+        transform.rotation = plaerShip.transform.rotation;
+    }
+}
+
+
---
+++ Glitchscape01_Cherry/Assets/_Scripts/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    float speed = 1f;
+    float rotSpeed = .5f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.forward * speed);
+
+        if(Input.GetKey("left"))
+        {
+            transform.Rotate(Vector3.forward * rotSpeed);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(Vector3.forward * -rotSpeed);
+        }
+
+        if(Input.GetKey("up"))
+        {
+            transform.Rotate(Vector3.left * -rotSpeed);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            transform.Rotate(Vector3.left * rotSpeed);
+        }
+    }
+}
